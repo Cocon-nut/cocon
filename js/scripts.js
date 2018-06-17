a4605eb05a727e18d25c420d40c0db375ee756f7
forwow495800@ya.ru <forwow495800@ya.ru>
2018-06-17 12:37:12 +0500

    js дерьмо

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// function pidor(a, b) {
+
+//     return a + b
+
+// }
+// alert(pidor(1, 2))
+// var p = "";
+// document.getElementById("text").innerHTML = p;
+// function handleInput() {
+//   var v = document.getElementById("vvod").value;
+//   document.getElementById("par").innerText = v;
+// }
+function f1() {
+  document.getElementById("par").style.color = "#FFAAFF";
+}

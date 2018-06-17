3463c409c56cb0c525336b2752c629193a3a6970
forwow495800@ya.ru <forwow495800@ya.ru>
2018-06-17 21:56:59 +0500

    масстивы

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,35 @@
 //   var v = document.getElementById("vvod").value;
 //   document.getElementById("par").innerText = v;
 // }
+// function f1() {
+//   var p;
+//   p = document.getElementById("par");
+//   var i = 0;
+//   while (i < 50) {
+//     i++;
+//     p.innerHTML += i + " ";
+//   }
+// }
+var mas = [122, 456, 377];
+
+function show() {
+  var p = document.getElementById("par");
+  var str = "";
+
+  for (var i = 0; i < mas.length; i++) {
+    str += i + "  )  " + mas[i] + "<br>";
+  }
+
+  p.innerHTML = str;
+}
+
+show();
 function f1() {
-  document.getElementById("par").style.color = "#FFAAFF";
+  var i1 = document.getElementById("i1").value;
+  mas.push(i1);
+  show();
+}
+function f2() {
+  mas.pop();
+  show();
 }
